bb08ab09600d02fb88f169b2ce5c251218a0347b
Huseyin Uztetik <husuzttemu@gmail.com>
2018-12-22 03:35:39 +0300

    commit before form added on post-create

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component,
+         EventEmitter,
+         Output  } from '@angular/core';
+
+import { Post } from '../post.model';
 
 @Component({
 selector : 'app-post-create',
@@ -9,14 +13,21 @@ styleUrls :['./post-create.component.css']
 
 export class PostCreateComponent{
 
-  enteredValue = '';
-  newPost = 'NO CONTENT';
+  enteredContent = '';
+  enteredTitle = '';
+
+
+  @Output() postCreated  = new EventEmitter<Post>();
 
   onAddPost()
   {
     // console.dir(postInput);   onAddPost(postInput: HTMLTextAreaElement)
     // this.newPost = postInput.value;
-    this.newPost = this.enteredValue;
+    // this.newPost = this.enteredValue;
+
+    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+
+    this.postCreated.emit(post);
   }
 
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component ,Input} from '@angular/core';
+import { Post } from '../post.model';
 
 @Component({
   selector : 'app-post-list',
@@ -6,9 +7,13 @@ import { Component } from '@angular/core';
   styleUrls : ['./post-list.component.css']
 })
 export class PostListComponent {
+  /*
   posts = [
   {title : 'first post' , content : 'This is the first post"s content'},
   {title : 'Second post' , content : 'This is the second post"s content'},
   {title : 'Third post' , content : 'This is the thisrd post"s content'}
-]
+]*/
+
+@Input() posts: Post[] = [];
+
 }
